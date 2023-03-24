93ef95059eb14004af048eec93093337f9f6749b
Kyle Ledin <Kyle.Ledin85@gmail.com>
2023-03-24 11:52:04 -0500

    Changes CI test to pass

--- spec/CI_test.rb
+++ spec/CI_test.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "CI test", type: :request do
-  it "should pass" do
-    expect(true).to be false
+  it "should not pass" do
+    expect(true).to be true
   end
 end
