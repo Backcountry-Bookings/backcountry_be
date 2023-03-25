ca21554bc98b652fe222ef83843f345ffea2660a
Kerynn Davis <kerynn.davis@gmail.com>
2023-03-25 12:15:09 -0700

    campsite controller details and serializer

--- app/controllers/api/v1/campsites_controller.rb
+++ app/controllers/api/v1/campsites_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::CampsitesController < ApplicationController
 
   def show 
-    
+    campsite = CampsiteFacade.get_campsite(params[:id])
+    render json: CampsiteSerializer.new(campsite)
   end
 end
---
+++ app/serializers/campsite_serializer.rb
@@ -0,0 +1,5 @@
+class CampsiteSerializer
+  include JSONAPI::Serializer
+
+  attributes :name, :id, :lat, :long, :booking_link, :description, :image_array, :cost, :number_of_reservation_sites, :reservation_info, :toilets, :showers, :cell_coverage, :laundry, :dump_station, :camp_store, :potable_water, :ice_available, :firewood_available, :wheelchair_access, :weather_info
+end
