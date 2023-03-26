2d9b896eec0231084eec5e10032f64ad7f0278e1
Kerynn Davis <kerynn.davis@gmail.com>
2023-03-26 10:10:29 -0700

    create favorite model and tests

---
+++ spec/models/favorite_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Favorite do 
+  describe 'validations' do 
+    it { should validate_presence_of :campsite_name }
+    it { should validate_presence_of :campsite_id }
+  end
+
+  describe 'relationships' do
+    it { should belong_to :user }
+  end
+end
