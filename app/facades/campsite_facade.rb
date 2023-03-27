b318283464ea9a05ce299bb029ad968ebb64d9df
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-03-27 15:00:10 -0600

    Add #get_campsites_by_state method to search for campsites by state code

--- app/facades/campsite_facade.rb
+++ app/facades/campsite_facade.rb
@@ -4,4 +4,11 @@ class CampsiteFacade
     campsite_data = CampsiteService.find_by_camp(camp_id)
     Campsite.new(campsite_data[:data][0])
   end
+
+  def self.get_campsites_by_state(state_code)
+    campsite_data = CampsiteService.find_by_state(state_code)
+    campsite_data[:data].map do |campsite|
+      CampsiteSearch.new(campsite)
+    end
+  end
 end
