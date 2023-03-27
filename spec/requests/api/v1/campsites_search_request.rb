4a6265a7f0c1d46990646c44049ce215f621ecfd
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-03-27 16:12:19 -0600

    Add test for #get_campsites_by_name request; Remove id from test expectations; Pass test

--- spec/requests/api/v1/campsites_search_request.rb
+++ spec/requests/api/v1/campsites_search_request.rb
@@ -4,6 +4,9 @@ RSpec.describe 'CampsitesSearch API' do
   before :each do
     stub_request(:get, "https://developer.nps.gov/api/v1/campgrounds?api_key=#{ENV['NPS_API_KEY']}&stateCode=CO")
       .to_return(status: 200, body: File.read('./spec/fixtures/campsites_search_by_state.json'), headers: {})
+  
+    stub_request(:get, "https://developer.nps.gov/api/v1/campgrounds?api_key=#{ENV['NPS_API_KEY']}&q=Rocky")
+      .to_return(status: 200, body: File.read('./spec/fixtures/campsites_search_by_name.json'), headers: {})
   end
 
   describe 'return list of campsites by state' do
@@ -25,7 +28,33 @@ RSpec.describe 'CampsitesSearch API' do
         expect(campsite).to have_key(:attributes)
         expect(campsite[:attributes]).to be_a(Hash)
         expect(campsite[:attributes]).to have_key(:name)
-        expect(campsite[:attributes]).to have_key(:id)
+        expect(campsite[:attributes]).to have_key(:description)
+        expect(campsite[:attributes]).to have_key(:images)
+        expect(campsite[:attributes][:images]).to be_an(Array)
+      end
+    end
+  end
+
+  describe '#get_campsites_by_name' do
+    it 'returns the campsites by name' do
+      get '/api/v1/campsites?q=Rocky'
+
+      expect(response).to be_successful
+
+      campsite_details = JSON.parse(response.body, symbolize_names: true)
+
+      expect(campsite_details).to be_a(Hash)
+      expect(campsite_details).to have_key(:data)
+      expect(campsite_details[:data]).to be_an(Array)
+      expect(campsite_details[:data].count).to eq(2)
+      
+      campsite_details[:data].each do |campsite|
+        expect(campsite).to have_key(:id)
+        expect(campsite).to have_key(:type)
+        expect(campsite[:type]).to eq('campsite_search')
+        expect(campsite).to have_key(:attributes)
+        expect(campsite[:attributes]).to be_a(Hash)
+        expect(campsite[:attributes]).to have_key(:name)
         expect(campsite[:attributes]).to have_key(:description)
         expect(campsite[:attributes]).to have_key(:images)
         expect(campsite[:attributes][:images]).to be_an(Array)
