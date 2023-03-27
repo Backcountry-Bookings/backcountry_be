4e7435a7c9545e1d58fbe5417a94071934febc3b
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-03-27 17:27:08 -0600

    Add park_code to list of attributes

--- app/poros/campsite_search.rb
+++ app/poros/campsite_search.rb
@@ -1,10 +1,11 @@
 class CampsiteSearch
-  attr_reader :name, :id, :description, :images
+  attr_reader :name, :id, :description, :images, :park_code
 
   def initialize(data)
     @name = data[:name]
     @id = data[:id]
     @description = data[:description]
     @images = data[:images]
+    @park_code = data[:parkCode]
   end
 end
