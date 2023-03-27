9ec19d014a512f816088de4b22b390663b217f8a
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-03-27 15:04:23 -0600

    Add index action for CampsitesController

--- app/controllers/api/v1/campsites_controller.rb
+++ app/controllers/api/v1/campsites_controller.rb
@@ -1,4 +1,12 @@
 class Api::V1::CampsitesController < ApplicationController
+  def index
+    if params[:state_code]
+      campsites = CampsiteFacade.get_campsites_by_state(params[:state_code])
+    elsif params[:q]
+      campsites = CampsiteFacade.get_campsites_by_search(params[:q])
+    end
+    render json: CampsiteSearchSerializer.new(campsites)
+  end
 
   def show 
     campsite = CampsiteFacade.get_campsite(params[:id])
