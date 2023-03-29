33a2b44a260687f47e8e1f2ec1576e6671086170
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-03-28 20:06:10 -0600

    Add tests for destroy action in Favorites requests

--- spec/requests/api/v1/favorite_request_spec.rb
+++ spec/requests/api/v1/favorite_request_spec.rb
@@ -76,4 +76,45 @@ RSpec.describe "Favorite" do
       expect(message[:errors]).to eq("User not found")
     end
   end
+
+  describe 'Favorite Delete', type: :request do
+    it 'successfully deletes a favorite' do
+      user = User.create!(name: "Robert", id: 1)
+
+      favorite1 = Favorite.create!(campsite_name: "Aspenglen Campground", campsite_id: "7475825B-E844-4012-841B-0E29E05D4540", campsite_details: "Aspenglen Campground is reservation only. Visit Recreation.gov.", image: "https://www.nps.gov/romo/planyourvisit/agcg.htm", user_id: user.id)
+      favorite2 = Favorite.create!(campsite_name: "Rocky Mountain Campground", campsite_id: "7ADFe25B-E444-1112-841B-0ABC4367823", campsite_details: "BEST CAMPGROUND EVER.", image: "https://www.nps.gov/romo/planyourvisit/agcg.htm", user_id: user.id)
+
+      expect(user.favorites.count).to eq(2)
+
+      delete "/api/v1/favorites/#{favorite1.id}"
+
+      expect(user.favorites.count).to eq(1)
+      expect(user.favorites.first).to eq(favorite2)
+
+      message = JSON.parse(response.body, symbolize_names: true)
+
+      expect(message).to be_a(Hash)
+      expect(message).to have_key(:success)
+      expect(message[:success]).to eq("Favorite deleted successfully")
+    end
+
+    it 'returns an error if favorite is not found' do
+      user = User.create!(name: "Robert", id: 1)
+
+      favorite1 = Favorite.create!(campsite_name: "Aspenglen Campground", campsite_id: "7475825B-E844-4012-841B-0E29E05D4540", campsite_details: "Aspenglen Campground is reservation only. Visit Recreation.gov.", image: "https://www.nps.gov/romo/planyourvisit/agcg.htm", user_id: user.id)
+      favorite2 = Favorite.create!(campsite_name: "Rocky Mountain Campground", campsite_id: "7ADFe25B-E444-1112-841B-0ABC4367823", campsite_details: "BEST CAMPGROUND EVER.", image: "https://www.nps.gov/romo/planyourvisit/agcg.htm", user_id: user.id)
+
+      expect(user.favorites.count).to eq(2)
+
+      delete "/api/v1/favorites/99999"
+
+      expect(user.favorites.count).to eq(2)
+      
+      message = JSON.parse(response.body, symbolize_names: true)
+
+      expect(message).to be_a(Hash)
+      expect(message).to have_key(:errors)
+      expect(message[:errors]).to eq("Favorite not found")
+    end
+  end
 end
