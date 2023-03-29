09f3d372f59628629a592a68c0e5eb9f62cdc216
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-03-28 19:13:54 -0600

    Add resources for favorites actions

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,7 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do
       resources :campsites, only: :show
-      resources :users, only: :show do
-        resources :favorites, only: :create
-      end
+      resources :favorites, only: :create
     end
   end
 end
