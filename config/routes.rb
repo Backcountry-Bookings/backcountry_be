4e2326381c3c46265b9822eef9489eb8124c2057
Kyle Ledin <Kyle.Ledin85@gmail.com>
2023-03-29 18:29:48 -0500

    Fixes campsites route to have index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do 
     namespace :v1 do
-      resources :campsites, only: :show
+      resources :campsites, only: [:show, :index]
       resources :favorites, only: [:index, :create, :destroy]
     end
   end
