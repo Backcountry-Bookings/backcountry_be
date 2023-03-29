42686ead9ee11a83c5e23f40a9ee3d0c7c6bcba6
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-03-28 19:14:38 -0600

    Modify Favorite model to validate presence of campsite image and details

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,5 +1,5 @@
 class Favorite < ApplicationRecord
   belongs_to :user
 
-  validates_presence_of :campsite_name, :campsite_id
+  validates_presence_of :campsite_name, :campsite_id, :campsite_details, :image
 end
