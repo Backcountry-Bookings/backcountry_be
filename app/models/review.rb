7ebde4eebf898d212a8f5c8b1aa8cf65836a514a
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-03-31 17:13:30 -0600

    Create Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,13 @@
 class Review < ApplicationRecord
-  # validates_presence_of :campsite_id
+  validates_presence_of :campsite_id
   belongs_to :user
   has_one_attached :img_file
+
+  def image_url
+    if img_file.attached?
+      self.img_file.service_url
+    else
+      "No Image"
+    end
+  end
 end
