880283bdfe5171738126b95dbf78a2b1bfe300f9
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-03-31 17:23:55 -0600

    Remove Image files

--- app/models/image.rb
+++
@@ -1,8 +0,0 @@
-class Image < ApplicationRecord
-  belongs_to :review
-  has_one_attached :img_file
-
-  def pic_url
-    self.img_file.attachment.url
-  end
-end
--- app/serializers/image_serializer.rb
+++
@@ -1,6 +0,0 @@
-class ImageSerializer
-  include JSONAPI::Serializer
-
-  attributes :id, :pic_url
-  belongs_to :review
-end
