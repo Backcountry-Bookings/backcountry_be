3f789786052ac6c07ec57d0fe4d2c6dc5a68b4e0
Kerynn Davis <kerynn.davis@gmail.com>
2023-04-03 16:48:10 -0700

    update review routes to have destroy

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :campsites, only: [:show, :index]
       resources :favorites, only: [:index, :create, :destroy]
-      resources :reviews, only: [:index, :create]
+      resources :reviews, only: [:index, :create, :destroy]
     end
   end
 end
