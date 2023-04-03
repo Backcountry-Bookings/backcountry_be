43553e632fc6fc32733bcc0b37a054d9f825e466
Kyle Ledin <Kyle.Ledin85@gmail.com>
2023-04-03 14:33:38 -0500

    Removes :img_file from being required in a review

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ApplicationRecord
   validates_presence_of :campsite_id
   belongs_to :user
   has_one_attached :img_file
-  validates :img_file, attached: true,size: { between: 1.kilobyte..5.megabytes , message: 'is not given between size' }, content_type: ['image/png', 'image/jpg', 'image/jpeg']
+  validates :img_file, size: { between: 1.kilobyte..5.megabytes , message: 'is not given between size' }, content_type: ['image/png', 'image/jpg', 'image/jpeg']
 
   def image_url
     if img_file.attached?
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -8,7 +8,6 @@ end
 RSpec.describe Review do 
   describe 'validations' do 
     it { should validate_presence_of :campsite_id }
-    it { is_expected.to validate_attached_of(:img_file) }
     it { is_expected.to validate_content_type_of(:img_file).allowing('image/png', 'image/jpg', 'image/jpeg') }
     it { is_expected.to validate_content_type_of(:img_file).rejecting('text/plain', 'text/xml', 'image/gif',) }
     it { is_expected.to validate_size_of(:img_file).between(1.kilobytes..5.megabytes) }
