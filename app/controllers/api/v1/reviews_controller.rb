291cf27e75e57718cf5b24f56e002fa111bea1fe
Kerynn Davis <kerynn.davis@gmail.com>
2023-04-04 14:14:38 -0700

    update review controller to validate file size before uploading to aws bucket

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,4 +1,10 @@
 class Api::V1::ReviewsController < ApplicationController
+  FILE_VALIDATIONS = {
+    content_types: ["image/jpeg", "image/png"],
+    max_size: 5.megabytes,
+    min_size: 1.kilobyte
+  }
+
   def index
     if reviews = Review.all.find_all{ |review| review.campsite_id == params[:campsite_id] }
       render json: ReviewSerializer.new(reviews)
@@ -13,12 +19,23 @@ class Api::V1::ReviewsController < ApplicationController
     
     user = User.find_by(id: params[:user_id])
     
-    review = user.reviews.new(review_params)
+    if params[:img_file].present?
+      if (params[:img_file].size > FILE_VALIDATIONS[:max_size]) || (params[:img_file].size < FILE_VALIDATIONS[:min_size])
+        render json: { error: "Img file is not given between size" }, status: 400
+      elsif review = user.reviews.new(review_params)
+        if review.save
+          render json: { success: 'Review saved' }, status: 201
+        else
+          render json: { error: review.errors.full_messages.to_sentence }, status: 400
+        end
+      end
     
-    if review.save
-      render json: { success: 'Review saved' }, status: 201
-    else
-      render json: { error: review.errors.full_messages.to_sentence }, status: 400
+    elsif review = user.reviews.new(review_params)
+      if review.save
+        render json: { success: 'Review saved' }, status: 201
+      else
+        render json: { error: review.errors.full_messages.to_sentence }, status: 400
+      end
     end
   end
 
