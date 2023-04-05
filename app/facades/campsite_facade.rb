c8db39b92cdd71fe34357cc5777647d9cb1ae333
Kerynn Davis <kerynn.davis@gmail.com>
2023-04-05 13:14:13 -0700

    campsites search by location method working in campsite facade

--- app/facades/campsite_facade.rb
+++ app/facades/campsite_facade.rb
@@ -2,7 +2,7 @@ class CampsiteFacade
 
   def self.get_campsite(camp_id)
     campsite_data = CampsiteService.find_by_camp(camp_id)
-    Campsite.new(campsite_data[:data][0])
+    CampsiteDetails.new(campsite_data[:data][0])
   end
 
   def self.get_campsites_by_state(state_code)
@@ -32,8 +32,14 @@ class CampsiteFacade
   def self.get_campsites_by_location(coords)
     format_coords = coords.split(',').map(&:to_f).first(2)
     campsites = Campsite.near([format_coords[0], format_coords[1]], 50).limit(10)
-    campsites.map do |campsite|
-      CampsiteLocationSearch.new(campsite)
+    campsite_ids = campsites.map do |campsite|
+      campsite.nps_id
+    end
+    campsite_data = campsite_ids.map do |id|
+      CampsiteService.find_by_name(id)
+    end
+    camps = campsite_data.map do |campsite|
+      CampsiteSearch.new(campsite[:data][0])
     end
   end
 end
